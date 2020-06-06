dd1d7f7657d7509206c8f52346dc729042e2f18a
jagjitsai22 <jagjitsai22@gmail.com>
2020-06-06 12:28:56 +0530

    Added: Password validation at list one upperletter functionality.

--- userRegistration.sh
+++ userRegistration.sh
@@ -13,9 +13,8 @@ emailPattern="^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.][a-zA-Z]{2,3}){
 
 phonePattern="^[0-9]{2}[[:space:]][0-9]{10}$"
 
-passwordPattern="^[a-zA-Z]{8,}$"
-
-nameErrorMsg="Invalid name format Name Start with Capital and minimum 3 characters."
+passwordPattern="^(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$"
+passwordPattern="[[${#s} -ge 6 && "$s" == *[A-Z]* && "$s" == *[a-z]* && "$s" == *[0-9]* ]]"
 
 function validate()
 {
@@ -28,20 +27,36 @@ function validate()
 	fi
 }
 
-read -p "Enter the first name: " name
-validate $name $namePattern "${nameErrorMsg}"
 
-read -p "Enter the Last name: " lname
-validate $lname $namePattern "${nameErrorMsg}"
+#read -p "Enter the first name: " name
+#nameErrorMsg="Invalid name format Name Start with Capital and minimum 3 characters."
+#validate $name $namePattern "${nameErrorMsg}"
 
-read -p "Enter the email Address: " email
-errorMessage="Invalid Email Please Enter the valid email address"
-validate $email $emailPattern "${errorMessage}"
+#read -p "Enter the Last name: " lname
+#validate $lname $namePattern "${nameErrorMsg}"
 
-read -p "Enter the phone number: " phoneNumber
-errorMessage="Invalid Phone Number.."
-validate "$phoneNumber" $phonePattern "${errorMessage}"
+#read -p "Enter the email Address: " email
+#errorMessage="Invalid Email Please Enter the valid email address"
+#validate $email $emailPattern "${errorMessage}"
+
+#read -p "Enter the phone number: " phoneNumber
+#errorMessage="Invalid Phone Number.."
+#validate "$phoneNumber" $phonePattern "${errorMessage}"
+
+
+## Regex password validation in Bash shell
+## https://stackoverflow.com/questions/20845497/regex-password-validation-in-bash-shell
+
+function validatePassword()
+{
+        if [[ ${#1} -ge 8 && "$1" == *[A-Z]* && "$1" == *[a-zA-Z]* ]]
+        then
+                echo "Correct"
+        else
+                echo "Wrong Password.."
+        fi
+}
 
 read -p "Enter the password:" password
 errorMessage="Invalid Password Enter minimum 8 character"
-validate $password $passwordPattern "${errorMessage}"
+validatePass $password

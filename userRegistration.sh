9c2733f9d6a9f5745c2097e05a03c59f528b4b2d
jagjitsai22 <jagjitsai22@gmail.com>
2020-06-06 11:10:39 +0530

    Added: Email Validation functionality.

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,10 +1,12 @@
 echo "Welcome to user registration.."
 shopt -s extglob
 
+
 #bash -O globasciiranges -c 'echo [A-Z]*'
 
 #namePattern="^[A-Z][a-z]{2,}$"
-namePattern="^[A-Z][a-z]{2,}$"
+namePattern="^[ABCDEFGHIJKLMNOPQRSTUVWXYZ][a-z]{2,}$"
+emailPattern="^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.][a-zA-Z]{2,3}){1,2}$"
 nameErrorMsg="Invalid name format Name Start with Capital and minimum 3 characters."
 
 function validate()
@@ -20,7 +22,9 @@ function validate()
 read -p "Enter the first name: " name
 validate $name $namePattern "${nameErrorMsg}"
 
-read -p "Enter the Last name: " name
-validate $name $namePattern "${nameErrorMsg}"
+read -p "Enter the Last name: " lname
+validate $lname $namePattern "${nameErrorMsg}"
 
-
+read -p "Enter the email Address: " email
+errorMessage="Invalid Email Please Enter the valid email address"
+validate $email $emailPattern "${errorMessage}"

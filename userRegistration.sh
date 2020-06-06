cf630d0a05b5d1aef5f6348a7c66aa3329d28acf
jagjitsai22 <jagjitsai22@gmail.com>
2020-06-06 10:25:13 +0530

    Added:First Validation Functinality.

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,3 +1,16 @@
 echo "Welcome to user registration.."
+#shot -s extglob
+
+#namePatt="^[A-Z][a-z]{2,}$"
+read -p "Enter the name: " name
+namePatt="^[A-Z]{1}[a-z]{2,}$"
+nameErrorMsg="Invalid name format First name Start with Capital and minimum 3 characters."
+
+if [[ $name =~ $namePatt ]]
+then
+  	echo "Yes"
+else
+       	echo nameErrorMsg
+fi
 
 

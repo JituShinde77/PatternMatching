e6619f6c346ab712bc48e43a5d81ea9e39661558
jagjitsai22 <jagjitsai22@gmail.com>
2020-06-06 11:35:08 +0530

    Added: Phone number validation functionality.

--- userRegistration.sh
+++ userRegistration.sh
@@ -5,13 +5,19 @@ shopt -s extglob
 #bash -O globasciiranges -c 'echo [A-Z]*'
 
 #namePattern="^[A-Z][a-z]{2,}$"
+
 namePattern="^[ABCDEFGHIJKLMNOPQRSTUVWXYZ][a-z]{2,}$"
+
 emailPattern="^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.][a-zA-Z]{2,3}){1,2}$"
+
+phonePattern="^[0-9]{2}[[:space:]][0-9]{10}$"
+
 nameErrorMsg="Invalid name format Name Start with Capital and minimum 3 characters."
 
 function validate()
 {
-	if [[ $1 =~ $2 ]]
+
+	if [[ ${1} =~ $2 ]]
 	then
   		echo "Yes"
 	else
@@ -28,3 +34,7 @@ validate $lname $namePattern "${nameErrorMsg}"
 read -p "Enter the email Address: " email
 errorMessage="Invalid Email Please Enter the valid email address"
 validate $email $emailPattern "${errorMessage}"
+
+read -p "Enter the phone number: " phoneNumber
+errorMessage="Invalid Phone Number.."
+validate "$phoneNumber" ${phonePattern} "${errorMessage}"

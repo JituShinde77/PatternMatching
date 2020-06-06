41c59f31d27c65a391d4c0649f2332b10980bd0a
JituShinde77 <shindejitu77@gmail.com>
2020-06-07 00:13:01 +0530

    Remove Comments

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,20 +1,9 @@
 echo "Welcome to user registration.."
 shopt -s extglob
 
-
-#bash -O globasciiranges -c 'echo [A-Z]*'
-
 namePattern="^[A-Z][a-z]{2,}$"
-
-#namePattern="^[ABCDEFGHIJKLMNOPQRSTUVWXYZ][a-z]{2,}$"
-#namePattern="^[[:upper:]][a-z]{2,}$"
-
 emailPattern="^[a-zA-Z0-9]+([._+-][a-zA-Z0-9]+)*@[a-zA-Z0-9]+([.][a-zA-Z]{2,3}){1,2}$"
-
 phonePattern="^[0-9]{2}[[:space:]][0-9]{10}$"
-
-#passwordPattern="^(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$"
-#passwordPattern="^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)\S{6,}$"
 
 # Validation Function
 function validate()
@@ -44,7 +33,6 @@ read -p "Enter the phone number: " phoneNumber
 errorMessage="Invalid Phone Number.."
 validate "$phoneNumber" $phonePattern "${errorMessage}"
 
-
 ## Regex password validation in Bash shell
 ## https://stackoverflow.com/questions/20845497/regex-password-validation-in-bash-shell
 
@@ -62,5 +50,4 @@ function validatePassword()
 
 read -p "Enter the password:" password
 errorMessage="Invalid Password Enter minimum 8 character"
-#validate $password $passwordPattern "${errorMessage}"
 validatePassword $password
